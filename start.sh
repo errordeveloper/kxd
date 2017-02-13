6b152dde9c3ef0ce8eb1addfcf2b2f81e13665db
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-02-13 18:44:49 +0000

    Fix issue with /var/lib/kubelet

--- start.sh
+++ start.sh
@@ -5,22 +5,35 @@ set -o pipefail
 set -o nounset
 
 volumes=(
-  /var/lib/docker:/var/lib/docker:rw
   /:/rootfs:ro
   /sys:/sys:ro
   /dev:/dev:rw
   /var/run:/var/run:rw
+  /var/lib:/var/lib:rw
+  /etc/kubernetes:/etc/kubernetes:rw
 )
 
-#"/var/lib/kubelet:/var/lib/kubelet:rw,rshared"
 #"/var/log/containers:/var/log/containers:rw"
-#"/etc/kubernetes:/etc/kubernetes:rw"
 
-args=(
-  --rm --interactive --tty 
+kill_list=($(docker ps --all --quiet))
+
+test "${#kill_list}" -gt 0 && docker rm --volumes --force "${kill_list[@]}"
+
+commond_args=(
   --privileged --net=host --pid=host
 )
 
-for v in "${volumes[@]}" ; do args+=("--volume=${v}") ; done
+for v in "${volumes[@]}" ; do commond_args+=("--volume=${v}") ; done
 
-docker run "${args[@]}" kdx
+## TODO this will fail on the first run for numerous reasons
+# - directories missing in /, so Docker for Mac will refuse mounts
+# - does kubeadm reset fail miserably?
+docker run --rm "${commond_args[@]}" kxd:shell "kubeadm reset"
+docker run --rm "${commond_args[@]}" kxd:shell "mkdir -p /var/lib/kubelet"
+docker run --rm "${commond_args[@]}" kxd:shell "nsenter --mount=/proc/1/ns/mnt -- mount --bind /var/lib/kubelet /var/lib/kubelet"
+docker run --rm "${commond_args[@]}" kxd:shell "nsenter --mount=/proc/1/ns/mnt -- mount --make-rshared /var/lib/kubelet"
+
+docker run --name=kxd --detach --volume=/var/lib/kubelet:/var/lib/kubelet:rw,rshared "${commond_args[@]}" kxd:kubelet
+
+docker exec --tty --interactive kxd kubeadm init --skip-preflight-checks
+docker exec --tty --interactive kxd kubectl create -f /etc/weave-daemonset.yaml
