f94ceefc3aa3eb18f86bed5d4865a22d823e6578
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-02-16 17:52:24 +0000

    Proxy natively on Docker for Mac

--- start.sh
+++ start.sh
@@ -13,6 +13,7 @@ readonly image_name="errordeveloper/kxd"
 sys_volumes=(
   /sys:/sys:ro
   /dev:/dev:rw
+  /port:/port:rw
   /var/run:/var/run:rw
   /var/lib:/var/lib:rw
 )
@@ -28,7 +29,7 @@ kxd_volumes=(
 readonly infra_label="kxd.k8s.io/infra=true"
 
 readonly kill_list=(
-  $(docker ps --all --filter "label=kxd.io.k8s/infra=true" --quiet)
+  $(docker ps --all --filter "label=${infra_label}" --quiet)
   $(docker ps --all --filter "label=io.kubernetes.pod.name" --quiet)
 )
 
@@ -65,18 +66,43 @@ fi
 
 readonly labels="--label=${infra_label}"
 
-docker run "${args[@]}" "${rootfs_vol}" --name=kxd --detach "${labels}" "${image_name}:kubelet"
+docker run "${args[@]}" "${rootfs_vol}" --name=kxd-kubelet --detach "${labels}" "${image_name}:kubelet"
 
 ## TODO it is possible Docker for Mac VM gets a different address on eth0
-readonly primary_address=192.168.65.2
-docker exec --tty --interactive kxd kubeadm init --skip-preflight-checks --api-advertise-addresses="${primary_address}" --api-advertise-addresses=127.0.0.1
-docker exec --tty --interactive kxd kubectl create --filename /etc/weave-daemonset.yaml
+readonly primary_address="192.168.65.2"
+readonly localhost="127.0.0.1"
+docker exec --tty --interactive kxd-kubelet kubeadm init --skip-preflight-checks --api-advertise-addresses="${primary_address}" --api-advertise-addresses="${localhost}"
+docker exec --tty --interactive kxd-kubelet kubectl create --filename /etc/weave-daemonset.yaml
 
-readonly proxy_port=8443
-docker run --detach --tty --interactive --publish="${proxy_port}:${proxy_port}" "${labels}" "${image_name}:shell" "socat TCP-LISTEN:${proxy_port},fork TCP:${primary_address}:6443"
+readonly proxy_port="6443"
+readonly kubernetes_service_ip="10.96.0.1"
+readonly kubernetes_service_port="443"
+readonly kubernetes_insecure_port="8080"
+readonly slirp_proxy_kubernetes_service=" \
+  /rootfs/usr/bin/slirp-proxy \
+    -i -no-local-ip -proto tcp \
+    -host-ip 0.0.0.0 -host-port ${proxy_port} \
+    -container-ip ${kubernetes_service_ip} -container-port ${kubernetes_service_port} \
+"
+readonly slirp_proxy_kubernetes_localhost=" \
+  /rootfs/usr/bin/slirp-proxy \
+    -i -no-local-ip -proto tcp \
+    -host-ip ${localhost} -host-port ${kubernetes_insecure_port} \
+    -container-ip ${localhost} -container-port ${kubernetes_insecure_port} \
+"
 
-docker cp kxd:/etc/kubernetes/admin.conf kubeconfig
+docker run "${args[@]}" "${rootfs_vol}" --name=kxd-api-proxy --detach "${labels}" \
+  --volume="/lib/ld-musl-x86_64.so.1:/lib/ld-musl-x86_64.so.1:ro" \
+  --volume="/lib/libc.musl-x86_64.so.1:/lib/libc.musl-x86_64.so.1:ro" \
+  "${image_name}:shell" "${slirp_proxy_kubernetes_service}"
+
+docker run "${args[@]}" "${rootfs_vol}" --name=kxd-api-proxy-insecure --detach "${labels}" \
+  --volume="/lib/ld-musl-x86_64.so.1:/lib/ld-musl-x86_64.so.1:ro" \
+  --volume="/lib/libc.musl-x86_64.so.1:/lib/libc.musl-x86_64.so.1:ro" \
+  "${image_name}:shell" "${slirp_proxy_kubernetes_localhost}"
+
+docker cp kxd-kubelet:/etc/kubernetes/admin.conf kubeconfig
 export KUBECONFIG=kubeconfig
-kubectl config set-cluster kubernetes --server="https://127.0.0.1:${proxy_port}"
+kubectl config set-cluster kubernetes --server="https://${localhost}:${proxy_port}"
 kubectl taint node moby dedicated:NoSchedule-
 kubectl get nodes
